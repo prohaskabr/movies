feacafda45bc0c358c7802c332856961b5f55882
Thiago Prohaska <thiago.prohaska@gmail.com>
2025-01-03 15:38:00 +0100

    Create Project

---
+++ Movies.Application/ApplicationServiceCollectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Movies.Application.Repositories;
+
+namespace Movies.Application;
+
+public static class ApplicationServiceCollectionExtensions
+{
+
+    public static IServiceCollection AddMoviesApplication(this IServiceCollection services)
+    {
+        services.AddSingleton<IMovieRepository, MovieRepository>();
+        return services;
+    }
+}
