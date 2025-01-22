0d4e6c7e16ef9f73fa18dd23d99c89e503a2e0ff
Thiago Prohaska <thiago.prohaska@gmail.com>
2025-01-22 07:54:32 +0100

    Create api sdk

---
+++ Movies.Api.Sdk.Consumer/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using System.Text.Json;
+using Movies.Api.Sdk;
+using Refit;
+
+var moviesApi = RestService.For<IMoviesApi>("http://localhost:5258");
+
+var movie = await moviesApi.GetMovieAsync("nosferatu-2024");
+
+Console.WriteLine(JsonSerializer.Serialize(movie));
+
+
+Console.ReadKey();
